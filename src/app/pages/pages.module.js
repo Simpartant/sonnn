b2293e03956407c9dcc88d6f7fa268e279fd4d3a
Son <hoangsonui0209@gmail.com>
2019-12-23 10:59:21 +0700

    dateCtr

--- src/app/pages/pages.module.js
+++ src/app/pages/pages.module.js
@@ -8,7 +8,7 @@
     // 'BlurAdmin.pages.dashboard',
     // 'BlurAdmin.pages.ui',
     // 'BlurAdmin.pages.components',
-    'BlurAdmin.pages.form',
+    // 'BlurAdmin.pages.form',
     // 'BlurAdmin.pages.tables',
     // // 'BlurAdmin.pages.charts',
     // // 'BlurAdmin.pages.maps',
